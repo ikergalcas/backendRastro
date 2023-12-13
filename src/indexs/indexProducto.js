6d4d0073fad846c6204e954d977ba5f4a7d039dc
Iker Gálvez Castillo <ikergalcas@gmail.com>
2023-12-13 12:17:09 +0100

    vercel

--- src/indexs/indexProducto.js
+++
@@ -1,8 +0,0 @@
-//Desde index.js arrancamos el backend
-
-import app from '../apps/appProducto.js'  //Como el fichero app.js es creado por nosotros debemos indicar la direccion
-import {connectDB} from '../db.js'   //Uso las llaves pq no he hecho export default
-
-connectDB()
-app.listen(3001)
-console.log('Server on port', 3001)
